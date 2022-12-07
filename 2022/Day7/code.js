f311e30e95537193efd6fa513561fc247e69995a
Rhys Stever <rhys@stever.com>
2022-12-07 11:53:18 -0500

    Started day 7

---
+++ 2022/Day7/code.js
@@ -0,0 +1,19 @@
+import { testInput, input } from "./input.js";
+
+const yearNum = 2022;
+const dayNum = 7;
+
+const inputF = input.split("\n");
+console.log(inputF);
+
+// ===== Part 1 =====
+
+var part1Answer = 0;
+// console.log("Part 1 answer is: " + part1Answer);
+
+// ===== Part 2 =====
+
+var part2Answer = 0;
+// console.log("Part 2 answer is: " + part2Answer);
+
+export { yearNum, dayNum, part1Answer, part2Answer };
