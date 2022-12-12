dc1a336164b2a3c421d049c3718510fa46ecfe60
Rhys Stever <rhys@stever.com>
2022-12-12 18:31:11 -0500

    Started day 10 part 2

--- 2022/Day10/code.js
+++ 2022/Day10/code.js
@@ -1,7 +1,7 @@
 import { input, testInput } from "./input.js";
 
-const yearNum = 2000;
-const dayNum = 0;
+const yearNum = 2022;
+const dayNum = 10;
 
 function formatInput(input) {
   var formattedInput = [];
@@ -18,7 +18,7 @@ const inputF = formatInput(input);
 
 // ===== Part 1 =====
 
-function findSignal(input, index) {
+function findSum(input, index) {
   var sum = 1;
 
   for (var i = 0; i < index - 1; i++) {
@@ -27,13 +27,13 @@ function findSignal(input, index) {
       sum += num;
     }
   }
-  return sum * index;
+  return sum;
 }
 
 function sumSignals(input, indices) {
   var total = 0;
   indices.forEach((index) => {
-    total += findSignal(input, index);
+    total += findSum(input, index) * index;
   });
   return total;
 }
@@ -43,6 +43,37 @@ var part1Answer = sumSignals(inputF, indices);
 console.log("Part 1 answer is: " + part1Answer);
 
 // ===== Part 2 =====
+
+function findPixelChar(cycle, pos) {
+  var sprite = Array(40);
+  sprite.fill(".");
+  
+  var trunkedCycle = cycle % 40;
+  sprite[trunkedCycle] = "#";
+  sprite[trunkedCycle + 1] = "#";
+  sprite[trunkedCycle + 2] = "#";
+
+  console.log("Cycle: " + cycle + " Trunked Cycle: " + trunkedCycle + " Pos: " + pos + " Sprite: " + sprite[pos])
+
+  return sprite[pos];
+}
+
+function findLine(input) {
+  var str = "";
+  var total = 1;
+  for(var i = 0; i < 40; i++) {
+    str += findPixelChar(i, total);
+    console.log(findSignal(input, i));
+    total = findSignal(input, i);
+  }
+  return str;
+}
+
+var index = 4;
+for(var i = 0; i < index; i++) {
+  var total = findSum(testInputF, i + 2);
+  findPixelChar(i, total);
+}
 
 var part2Answer = 0;
 // console.log("Part 2 answer is: " + part2Answer);
