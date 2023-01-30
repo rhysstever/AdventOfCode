22ccbde40b2b0396857e930b32fede3761a514bd
Stever, Rhys <Rhys_Stever@comcast.com>
2023-01-30 12:31:29 -0500

    More work on Day 7

--- AOC2015/app/src/main/java/com/android/aoc2015/AOCDay7.kt
+++ AOC2015/app/src/main/java/com/android/aoc2015/AOCDay7.kt
@@ -20,7 +20,7 @@ fun AOCDay7() {
 
     Row(horizontalArrangement = Arrangement.spacedBy(4.dp)) {
         Text(text = "Day $dayNum:")
-        AOCDay7Part1(day7RawInput)
+        AOCDay7Part1(day7TestInput)
         Text(text = "|")
         AOCDay7Part2("")
     }
@@ -35,64 +35,98 @@ fun formatInput(input: List<String>): List<List<String>> {
     return formattedInput
 }
 
-fun parseInput(input: List<List<String>>): MutableMap<String, Int> {
-    val map = mutableMapOf<String, Int>()
+fun setValues(instructionList: List<List<String>>): Map<String, String> {
+    var map = mutableMapOf<String, String>()
 
-    input.forEach {
-        if(it[0].contains("AND")) {
-            val num1 = map[it[0].split(" AND ")[0]]
-            val num2 = map[it[0].split(" AND ")[1]]
-            val newVal = num1!!.and(num2!!)
-            Log.v("AOC Answer: AND", "${it[1]}: $newVal")
-            map[it[1]] = newVal
-        }
-        else if(it[0].contains("OR")) {
-            val num1 = map[it[0].split(" OR ")[0]]
-            val num2 = map[it[0].split(" OR ")[1]]
-            val newVal = num1!!.or(num2!!)
-            Log.v("AOC Answer: OR", "${it[1]}: $newVal")
-            map[it[1]] = newVal
-        }
-        else if(it[0].contains("LSHIFT")) {
-            val key = it[0].split(" LSHIFT ")[0]
-            val keyVal = map[key]
-            val shiftNum = it[0].split(" LSHIFT ")[1].toInt()
-            val newVal = keyVal!! shl shiftNum
-            Log.v("AOC Answer: LSHIFT", "${it[1]}: $newVal")
-            map[it[1]] = newVal
-        }
-        else if(it[0].contains("RSHIFT")) {
-            val key = it[0].split(" RSHIFT ")[0]
-            val keyVal = map[key]
-            val shiftNum = it[0].split(" RSHIFT ")[1].toInt()
-            val newVal = keyVal!! shr shiftNum
-            Log.v("AOC Answer: RSHIFT", "${it[1]}: $newVal")
-            map[it[1]] = newVal
-        }
-        else if(it[0].contains("NOT")) {
-            val key = it[0].substring(4)
-            val keyVal = map[key]
-            val newVal = keyVal!!.inv()
-            Log.v("AOC Answer: NOT", "${it[1]}: $newVal")
-            map[it[1]] = newVal
-        }
-        else if(it[0].toIntOrNull() != null) {
-            val newVal = it[0].toInt()
-            Log.v("AOC Answer: Set", "${it[1]}: $newVal")
-            map[it[1]] = newVal
-        }
+    instructionList.forEach {
+        map[it[1]] = it[0]
     }
 
     Log.v("AOC Answer", map.toString())
     return map
 }
 
+fun findAssignment(instructionList: List<List<String>>, key: String): String {
+    instructionList.forEach {
+        if(it[1] == key)
+            return it[0]
+    }
+
+    return "No Assignment was found"
+}
+
+fun isOperation(instruction: String): Boolean {
+    return (instruction.contains("AND")
+        || instruction.contains("OR")
+        || instruction.contains("SHIFT")
+        || instruction.contains("NOT"))
+}
+
+//fun processOperation(map: Map<String, String>, instruction: String): String {
+//    if(instruction.contains("AND")) {
+//        val split = instruction.split(" AND ")
+//        val val1 = split[0]
+//        val key2 = split[2]
+//        return
+//    }
+//    else if(instruction.contains("OR"))
+//        return instruction.split(" OR ")
+//    else if(instruction.contains("LSHIFT"))
+//        return instruction.split(" LSHIFT ")
+//    else if(instruction.contains("RSHIFT"))
+//        return instruction.split(" RSHIFT ")
+//    else if(instruction.contains("NOT"))
+//        return instruction.split(" NOT ")
+//    else
+//        return listOf(instruction, "")
+//}
+
+fun process(map: Map<String, String>) {
+    map.forEach {
+        if(isOperation(it.value)) {
+//            val strArr = processOperation(map, it.value)
+//            Log.v("AOC Answer", strArr.toString())
+        } else {
+            val key = it.key
+            var newVal = it.value
+            var mapVal = map[it.value]
+            if(mapVal != null)
+                newVal = mapVal
+            Log.v("AOC Answer", "$key: $newVal")
+        }
+    }
+}
+
+fun andOperation(keyVal1: Int, keyVal2: Int): Int {
+    return keyVal1.and(keyVal2)
+}
+
+fun orOperation(keyVal1: Int, keyVal2: Int): Int {
+    return keyVal1.or(keyVal2)
+}
+
+fun shiftLeftOperation(keyVal: Int, shiftAmount: Int): Int {
+    return keyVal shl shiftAmount
+}
+
+fun shiftRightOperation(keyVal: Int, shiftAmount: Int): Int {
+    return keyVal shr shiftAmount
+}
+
+fun notOperation(keyVal: Int): Int {
+    return keyVal.inv().and(65535)
+}
+
 @Composable
 fun AOCDay7Part1(input: List<String>) {
     val formattedInput = formatInput(input)
-    val parsedInput = parseInput(formattedInput)
+    val map = setValues(formattedInput)
+    process(map)
 
-    Text(text = "Part 1 = ${parsedInput["a"]}")
+//    val test = process(formattedInput, "d")
+//    Log.v("AOC Answer: Result", "$test")
+
+//    Text(text = "Part 1 = ${parsedInput["a"]}")
 }
 
 @Composable
