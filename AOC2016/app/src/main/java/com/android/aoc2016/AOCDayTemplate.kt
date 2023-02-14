f80d99e4854dcb2eaff7dad5b5a511ce2c8a7747
Stever, Rhys <Rhys_Stever@comcast.com>
2023-02-14 09:53:34 -0500

    Update AOCDayTemplate.kt

--- AOC2016/app/src/main/java/com/android/aoc2016/AOCDayTemplate.kt
+++ AOC2016/app/src/main/java/com/android/aoc2016/AOCDayTemplate.kt
@@ -1,5 +1,6 @@
 package com.android.aoc2016
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
 import androidx.compose.material.Text
@@ -31,6 +32,7 @@ fun AOCDayTemp() {
 fun AOCDayTempPart1(input: String) {
     val part1Answer = 0
 
+    Log.v("Answer", "Part 1 Answer: $part1Answer")
     Text(text = "Part 1 = $part1Answer")
 }
 
@@ -38,5 +40,6 @@ fun AOCDayTempPart1(input: String) {
 fun AOCDayTempPart2(input: String) {
     val part2Answer = 0
 
+    Log.v("Answer", "Part 2 Answer: $part2Answer")
     Text(text = "Part 2 = $part2Answer")
 }
